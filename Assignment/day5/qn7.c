64c89c2a4f834b79b9c32c053420ccf080db05de
Amritha2512 <amri18106.ee@rmkec.ac.in>
2024-04-17 01:44:40 +0530

    Completed

---
+++ Assignment/day5/qn7.c
@@ -0,0 +1,32 @@
+//Print right triangle star pattern
+
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d",&n);
+    if(n<2||n>10)
+    {
+        printf("Invalid input. n should be 2 and 10.\n");
+        return 1;
+    }
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(int i=n-1;i>=1;i--)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
