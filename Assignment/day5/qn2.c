20a6c7a23d53de886accb3b3fc94330ba311539f
Amritha2512 <amri18106.ee@rmkec.ac.in>
2024-04-16 23:36:15 +0530

    Completed

---
+++ Assignment/day5/qn2.c
@@ -0,0 +1,16 @@
+//Print series 1,2,4,8,16,32,64. Given n=64
+
+#include<stdio.h>
+int main()
+{
+    int n=64;
+    int a;
+    int b=1;
+
+    for(a=0;b<=n;a++)
+    {
+        printf("%d ", b);
+        b*=2;
+    }
+    return 0;
+}
