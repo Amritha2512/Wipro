9f2fb57895638a2706890ab587420d4727773390
Amritha2512 <amri18106.ee@rmkec.ac.in>
2024-04-16 23:10:28 +0530

    Completed

---
+++ Assignment/day5/qn1.c
@@ -0,0 +1,16 @@
+//Print series 0,3,8,15,24,35,48,63,80,99 given n=10
+
+#include<stdio.h>
+
+int main()
+{
+    int n=10;
+    int a;
+    int b=0;
+    for(a=1;a<=n;a++)
+    {
+        b=a*a-1;
+        printf("%d ",b);
+    }
+    return 0;
+}
